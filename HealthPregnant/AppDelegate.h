e6e5a25b3098523dcebcb617a319e4a05359fcb8
liuhongli <liuhongli@liuhonglidemacbook-pro.local>
2016-03-31 11:40:36 +0800

    Initial Commit

---
+++ HealthPregnant/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HealthPregnant
+//
+//  Created by 刘宏立 on 16/3/31.
+//  Copyright © 2016年 honely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HealthPregnant/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HealthPregnant
+//
+//  Created by 刘宏立 on 16/3/31.
+//  Copyright © 2016年 honely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
