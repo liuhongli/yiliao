9f94c90a63bdfc433fa1a618873a13dd32dd3375
liuhongli <1071285570@qq.com>
2017-01-13 11:08:01 +0800

    个人中心

--- HealthPregnant/FourViewController.m
+++ HealthPregnant/FourViewController.m
@@ -13,6 +13,7 @@
     
     UITableView *tableV;
     NSArray *titleArray;
+    NSMutableDictionary *mymess;
 }
 
 @end
@@ -23,10 +24,24 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.title = @"个人信息";
-    
+    mymess = [NSMutableDictionary dictionary];
 
-    titleArray = @[@[@"手机号码",@"姓名",@"出生年月",@"身高",@"末次月经"],@[@"血型",@"名族",@"职业",@"E-Mail",@"文化程度"]];
+    titleArray = @[@[@"手机号码",@"姓名",@"出生年月",@"身高",@"末次月经"],@[@"血型",@"民族",@"职业",@"E-Mail",@"文化程度"]];
     [self initTableView];
+    
+    UIView *rightview = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 80, 40)];
+    
+    
+    UIButton *savebtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    savebtn.frame = CGRectMake(40, 0, 40, 40);
+    [savebtn setTitle:@"保存" forState:UIControlStateNormal];
+    [savebtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [savebtn addTarget:self action:@selector(savemess) forControlEvents:UIControlEventTouchUpInside];
+    [rightview addSubview:savebtn];
+    
+    UIBarButtonItem *buttonItem  = [[UIBarButtonItem alloc] initWithCustomView:rightview];
+    self.navigationItem.rightBarButtonItem  = buttonItem;
+
 }
 
 - (void)initTableView{
@@ -76,8 +91,13 @@
     NSArray *array = titleArray[indexPath.section];
     
     cell.titleLab.text = [NSString stringWithFormat:@"%@",array[indexPath.row]];
+    if ([mymess objectForKey:[NSString stringWithFormat:@"%ld", indexPath.section*10+indexPath.row+500]] ==nil) {
+        cell.subLab.text = @"未填写";
+    }else {
+        cell.subLab.text = [mymess objectForKey:[NSString stringWithFormat:@"%ld", indexPath.section*10+indexPath.row+500]];
+        cell.subLab.textColor =[UIColor colorWithHexString:@"FF8698" alpha:1];
+    }
     
-    cell.subLab.text = @"未填写";
     
     cell.selectionStyle = UITableViewCellSelectionStyleNone;
     
@@ -101,12 +121,50 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"编辑" message:@"请输入信息" delegate:self cancelButtonTitle:nil otherButtonTitles:@"确认", @"取消", nil];
-    
+    alertView.tag = 500+indexPath.section*10+indexPath.row;
     alertView.alertViewStyle = UIAlertViewStylePlainTextInput;
     [alertView show];
     
 }
 
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+    if (buttonIndex == 0) {
+        UITextField * accoutNameField = [alertView textFieldAtIndex:0];
+        NSLog(@"%@",accoutNameField.text);
+        [mymess setObject:accoutNameField.text forKey:[NSString stringWithFormat:@"%ld" ,(long)alertView.tag]];
+        [tableV reloadData];
+    }
+}
+
+//保存上传个人信息
+- (void)savemess{
+//http://test.kpjkgl.com:8090/pregnant/user/edit?mobilePhone=11111111&name=吴江&birthdate=1990-01-01&height=60&lastMenses=2016-01-01&bloodType=O&nationality=汉& profession=IT&email=161231233@qq.com&education=大学
+    
+    
+    [RBaseHttpTool postWithUrl:@"user/edit" parameters:mymess sucess:^(id json){
+        
+        if ([[json objectForKey:@"success"] floatValue]== 1) {
+            NSUserDefaults *userDetaults = [NSUserDefaults standardUserDefaults];
+            
+            [userDetaults setBool:YES forKey:@"showGuide"];
+            //将数据同步到本地的文件中
+            [userDetaults synchronize];
+            
+            [Alert showWithTitle:@"保存成功"];
+        }else{
+            [Alert showWithTitle:[NSString stringWithFormat:@"%@",[json objectForKey:@"message"]]];
+            
+        }
+        
+        
+    } failur:^(NSError *error) {
+        
+        [Alert showWithTitle:[NSString stringWithFormat:@"%@",error.userInfo]];
+        
+        
+    }];
+
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
